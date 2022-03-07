fcdfb266ce328632c9cb31709cc7879ade1d4fb6
Bo Andersen <Logifire@users.noreply.github.com>
2022-03-07 20:41:07 +0100

    Converted implementation to use object

--- deploy.php
+++ deploy.php
@@ -13,86 +13,95 @@ HELP;
     exit(0);
 }
 
-const CONFIG_PATH = 'deploy-config.json';
-
-function init(string $name, string $repository): void
+class Deploy
 {
 
-    $config = json_decode(@file_get_contents(CONFIG_PATH) ?: '[]', true);
-    $data = json_encode(
-        array_merge(
-            $config,
-            [$name => ['repository' => $repository]]
-        ),
-        JSON_PRETTY_PRINT
-    );
-    file_put_contents(CONFIG_PATH, $data);
-    chmod(CONFIG_PATH, 0644);
-}
+    private const CONFIG_PATH = 'deploy-config.json';
 
-// `$branch` can also be a tag
-function prepare(string $name, string $branch): void
-{
-    $directory = "{$name}-{$branch}";
 
-    $config = getConfigFor($name);
-    $repository = $config['repository'];
+    public function init(string $name, string $repository): void
+    {
 
-    $specific_branch = $branch ? "--branch {$branch} --single-branch" : '';
-
-    $command = "git clone --depth 1 {$specific_branch} -- {$repository} {$directory}";
-
-    exec($command);
-}
-
-// `$branch` can also be a tag
-function activate(string $name, string $branch): void
-{
-    getConfigFor($name); // validate name
-
-    $directory = "{$name}-{$branch}";
-
-    if (is_dir($directory) === false) {
-        echo "Invalid activation, run prepare first.";
-        exit(1);
+        $config = json_decode(@file_get_contents(self::CONFIG_PATH) ?: '[]', true);
+        $data = json_encode(
+            array_merge(
+                $config,
+                [$name => ['repository' => $repository]]
+            ),
+            JSON_PRETTY_PRINT
+        );
+        file_put_contents(self::CONFIG_PATH, $data);
+        chmod(self::CONFIG_PATH, 0644);
     }
 
-    $command = "ln --symbolic --force --no-dereference {$directory} {$name}";
+    // `$branch` can also be a tag
+    public function prepare(string $name, string $branch): void
+    {
+        $directory = "{$name}-{$branch}";
 
-    exec($command);
-}
+        $config = $this->getConfigFor($name);
+        $repository = $config['repository'];
 
-// TODO: scope this
-function getConfigFor(string $name): array
-{
-    $config = json_decode(@file_get_contents(CONFIG_PATH) ?: '[]', true);
+        $specific_branch = $branch ? "--branch {$branch} --single-branch" : '';
 
-    if (!array_key_exists($name, $config)) {
-        echo "Invalid name given: \"{$name}\". Hint: Run init command.", PHP_EOL;
-        exit(1);
+        $command = "git clone --depth 1 {$specific_branch} -- {$repository} {$directory}";
+
+        exec($command);
     }
 
-    return $config[$name];
+    // `$branch` can also be a tag
+    function activate(string $name, string $branch): void
+    {
+        $this->getConfigFor($name); // validate name
+
+        $directory = "{$name}-{$branch}";
+
+        if (is_dir($directory) === false) {
+            echo "Invalid activation, run prepare first.";
+            exit(1);
+        }
+
+        $command = "ln --symbolic --force --no-dereference {$directory} {$name}";
+
+        exec($command);
+    }
+
+    private function getConfigFor(string $name): array
+    {
+        $config = json_decode(@file_get_contents(self::CONFIG_PATH) ?: '[]', true);
+
+        if (!array_key_exists($name, $config)) {
+            echo "Invalid name given: \"{$name}\". Hint: Run init command.", PHP_EOL;
+            exit(1);
+        }
+
+        return $config[$name];
+    }
 }
+
+
 
 $command = @$argv[1];
 $arguments = array_slice($argv, 2);
 
-if (function_exists($command) === false) {
+$class = new ReflectionClass('Deploy');
+
+if ($class->hasMethod($command) === false) {
     echo "Invalid command: \"{$command}\". \nTry '{$script} --help' for more information. \n";
     exit(1);
 }
 
-$reflection = new ReflectionFunction($command);
-$number_required_params = $reflection->getNumberOfRequiredParameters();
+
+$method = $class->getMethod($command);
+$number_required_params = $method->getNumberOfRequiredParameters();
 
 if ($number_required_params > count($arguments)) {
     echo "Invalid number of arguments for \"{$command}\"; ";
-    foreach ($reflection->getParameters() as $parameter) {
+    foreach ($method->getParameters() as $parameter) {
         echo "<{$parameter->getName()}> ";
     }
     echo PHP_EOL;
     exit(1);
 }
 
-$command(...$arguments);
+(new Deploy)->$command(...$arguments);
